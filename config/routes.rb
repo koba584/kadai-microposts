be64bee3efa9b9047afa21ba5ac79d5dff865341
shumpei.kobashi <kobashun0709@gmail.com>
2018-02-25 06:16:15 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   root to: 'toppages#index'
+  
+  
+  get 'login', to: 'session#new'
+  post 'losin', to: 'session#create'
+  delete 'logout',to: 'session#destroy'
+  
+  
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
+  
 end
